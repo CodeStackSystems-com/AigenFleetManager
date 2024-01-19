8665c0ba475c4c591bbb3499d96ebf855e16651d
sam-gomez <samuel@codestacksystems.com>
2024-01-19 11:49:16 -0600

    orientation and typo fix

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home"; // Update this to match your actual file
 import JiraTickets from "./screens/JiraTickets";
-import {NativeModules, Button} from 'react-native';
+import { NativeModules, Button } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-const Stack = createStackNavigator();
-const {RobotManager} = NativeModules;
+const Stack = createNativeStackNavigator();
+const { RobotManager } = NativeModules;
 const NewModuleButton = () => {
   const onPress = async () => {
     const data = await RobotManager.getRobotHeartbeat();
@@ -18,16 +19,22 @@ const NewModuleButton = () => {
 };
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home as React.ComponentType} />
-        <Stack.Screen
-          name="JiraTickets"
-          component={JiraTickets}
-          options={{ title: "Jira Tickets" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home as React.ComponentType}
+            options={{ orientation: "landscape" }}
+          />
+          <Stack.Screen
+            name="JiraTickets"
+            component={JiraTickets}
+            options={{ title: "Jira Tickets", orientation: "landscape" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
